5445d2bbe47831dce5be4b0c754bcb0bc98a2f30
Jocelyn Legault <jocelynlegault@gmail.com>
2010-02-18 16:53:36 -0700

    Test runner: run in non-unicode.
    - Running the UNICODE runner caused problems with input
    characters.
    - The code was not returning the value of the unit tests
    run.
    
    [#35]
    
    Signed-off-by: Jocelyn Legault <jocelynlegault@gmail.com>

--- runner/runner.cpp
+++ runner/runner.cpp
@@ -1,13 +1,13 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 
-typedef int (__stdcall *runnerFunc)(int, TCHAR**);
-int main(int argc, TCHAR* argv[])
+typedef int (__stdcall *runnerFunc)(int, char**);
+int main(int argc, char* argv[])
 {
 	if (argc >= 2) {
 		if (HMODULE module = LoadLibrary(argv[1])) {
 			if (runnerFunc runner = (runnerFunc)::GetProcAddress (module, "_RunTests@8")) {
-				runner(argc, argv);
+				return runner(argc, argv);
 			}
 		}
 	}
